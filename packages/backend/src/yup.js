60fc7aaeb0faba80d6c86bb676017af7ab214c11
Andrés Pérez <andres.perez@galvintec.com>
2022-02-03 12:35:36 +0100

    Validate winner has to play the match

--- packages/backend/src/yup.js
+++ packages/backend/src/yup.js
@@ -1,11 +1,16 @@
-import { object, string, array } from 'yup';
+import { object, string, array, ref } from 'yup';
 
 let gameSchema = object({
   user1: string().required(),
   user2: string().required(),
-  winner: string().required(),
+  winner: string()
+    .required()
+    .oneOf(
+      [ref('user1'), ref('user2')],
+      'This players have not played the match!'
+    )
+    .required('Required'),
   resume: array().required()
-  //.when(['user1', 'user2', 'winner'], (user1, user2, winner, schema) => user1 === winner || user2=== winner})
 });
 
 export { gameSchema };
